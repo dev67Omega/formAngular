f730de7c55d23ec8fc9c3f98efc1482eec9405cc
dev67Omega <omegared@iMac-de-omegaRed.local>
2022-03-18 10:22:26 -0600

    formulario angular con validaciones

---
+++ forms/js/index.js
@@ -0,0 +1,12 @@
+var app = angular.module('todoApp', ['ngRoute']);
+
+// app.config(function($routeProvider){
+// 	$routeProvider
+// 	.when('/', {
+// 		// templateUrl: 'sites/login.html',
+// 		controller: 'mailCtrl'
+// 	})
+// 	.otherwise({
+// 		redirectTo: '/'
+// 	});
+// });
